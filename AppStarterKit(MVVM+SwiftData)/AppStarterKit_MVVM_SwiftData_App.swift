4ab5c32436c12975dc843f6c61dba710dffdfce7
Sabbir Hossain <sabbirsopnil07@gmail.com>
2025-11-27 15:17:01 +0600

    Initial Commit

---
+++ AppStarterKit(MVVM+SwiftData)/AppStarterKit_MVVM_SwiftData_App.swift
@@ -0,0 +1,17 @@
+//
+//  AppStarterKit_MVVM_SwiftData_App.swift
+//  AppStarterKit(MVVM+SwiftData)
+//
+//  Created by Sopnil Sohan on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AppStarterKit_MVVM_SwiftData_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
